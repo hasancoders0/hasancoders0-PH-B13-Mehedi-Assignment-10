aae47ec1af8332017f8565a3d88234bf2b2be2ca
Shopify <shopify.devs360@gmail.com>
2026-06-28 10:39:34 +0600

    implement doctor appointment request management

---
+++ src/services/prescription.service.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:5000";
+
+export const createPrescription = async (
+  prescriptionData
+) => {
+  const { data } = await axios.post(
+    `${API_URL}/api/prescriptions`,
+    prescriptionData
+  );
+
+  return data;
+};
+
+export const getPatientPrescriptions =
+  async (email) => {
+    const { data } = await axios.get(
+      `${API_URL}/api/prescriptions/patient/${email}`
+    );
+
+    return data.prescriptions;
+  };
